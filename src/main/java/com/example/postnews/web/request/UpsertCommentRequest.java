acc82f5ae6ab75cfa23e6b079f2ada9cfdc9d5e9
Alexey Yurchenko <alexeyyurchenko.java@gmail.com>
2024-03-07 10:55:12 +0700

    Upsert

--- src/main/java/com/example/postnews/web/request/UpsertCommentRequest.java
+++ src/main/java/com/example/postnews/web/request/UpsertCommentRequest.java
@@ -1,4 +1,24 @@
 package com.example.postnews.web.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class UpsertCommentRequest {
+
+    @NotNull(message = "User ID not specified")
+    private Long userId;
+
+    @NotNull(message = "Post ID not specified")
+    private Long postId;
+
+    @NotBlank(message = "Comment cannot be empty")
+    private String comment;
+
+
 }
--- src/main/java/com/example/postnews/web/request/UpsertPostRequest.java
+++ src/main/java/com/example/postnews/web/request/UpsertPostRequest.java
@@ -1,4 +1,29 @@
 package com.example.postnews.web.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class UpsertPostRequest {
+
+
+    @NotNull(message = "User ID not specified")
+    private Long userId;
+
+    @NotBlank(message = "The news title is required")
+    private String title;
+
+    private String description;
+
+    @NotBlank(message = "The news cannot be empty")
+    private String body;
+
+    @NotBlank(message = "News category name not specified")
+    private String categoryName;
+
 }

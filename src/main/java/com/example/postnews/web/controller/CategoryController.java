8c0e13e7dd8ac2b7491745f5a85aa93069b8e827
Alexey Yurchenko <alexeyyurchenko.java@gmail.com>
2024-03-10 15:00:31 +0700

    Last commit

--- src/main/java/com/example/postnews/web/controller/CategoryController.java
+++ src/main/java/com/example/postnews/web/controller/CategoryController.java
@@ -1,4 +1,56 @@
 package com.example.postnews.web.controller;
 
+import com.example.postnews.entity.Category;
+import com.example.postnews.exception.EntityNotFoundException;
+import com.example.postnews.mapper.CategoryMapper;
+import com.example.postnews.service.CategoryService;
+import com.example.postnews.web.request.UpsertCategoryRequest;
+import com.example.postnews.web.response.CategoryResponse;
+import com.example.postnews.web.response.list.CategoryListResponse;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.text.MessageFormat;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/post/category")
 public class CategoryController {
+
+    private final CategoryService categoryService;
+    private final CategoryMapper categoryMapper;
+
+    @GetMapping
+    public ResponseEntity<CategoryListResponse> findAll(@RequestParam int pageNumber, @RequestParam int pageSize) {
+        return ResponseEntity.ok(categoryMapper.categoryListToCategoryListResponse(categoryService.findAll(pageNumber,pageSize)));
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<CategoryResponse> findById(@PathVariable Long id) {
+        Category category = categoryService.findById(id);
+        if (category != null) {
+            return ResponseEntity.ok(categoryMapper.categoryToResponse(category));
+        }
+        throw new EntityNotFoundException(MessageFormat.format("Category with id= {0} not found", id));
+    }
+    @PostMapping
+    public ResponseEntity<CategoryResponse> create(@RequestBody @Valid UpsertCategoryRequest request) {
+        Category newCategory = categoryService.save(categoryMapper.requestToCategory(request));
+        return ResponseEntity.status(HttpStatus.CREATED).body(categoryMapper.categoryToResponse(newCategory));
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<CategoryResponse> update(@PathVariable("id") Long categoryId, UpsertCategoryRequest request) {
+        Category updateCategory = categoryService.update(categoryMapper.requestToCategory(categoryId,request));
+        if (updateCategory != null) {
+            return ResponseEntity.ok(categoryMapper.categoryToResponse(updateCategory));
+        }
+        throw new EntityNotFoundException(MessageFormat.format("Category with id= {0} not found", categoryId));
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteById(@PathVariable Long id) {
+        categoryService.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
 }
--- src/main/java/com/example/postnews/web/controller/UserController.java
+++ src/main/java/com/example/postnews/web/controller/UserController.java
@@ -4,9 +4,12 @@ import com.example.postnews.entity.User;
 import com.example.postnews.exception.EntityNotFoundException;
 import com.example.postnews.mapper.UserMapper;
 import com.example.postnews.service.UserService;
+import com.example.postnews.web.request.UpsertUserRequest;
 import com.example.postnews.web.response.UserResponse;
 import com.example.postnews.web.response.list.UserListResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -35,6 +38,23 @@ public class UserController {
         throw new EntityNotFoundException(MessageFormat.format("User with id= {0} not found", id));
     }
 
+    @PostMapping
+    public ResponseEntity<UserResponse> create(@RequestBody @Valid UpsertUserRequest request) {
+        User newUser = userService.save(userMapper.requestToUser(request));
+        return ResponseEntity.status(HttpStatus.CREATED).body(userMapper.userToResponse(newUser));
+    }
 
-
+    @PutMapping("/{id}")
+    public ResponseEntity<UserResponse> update(@PathVariable("id") Long userId, UpsertUserRequest request) {
+        User updateUser = userService.update(userMapper.requestToUser(userId,request));
+        if (updateUser !=null) {
+            return ResponseEntity.ok(userMapper.userToResponse(updateUser));
+        }
+        throw new EntityNotFoundException(MessageFormat.format("User with id= {0} not found", userId));
+    }
+    @DeleteMapping("?{id}")
+    public ResponseEntity<Void> deleteById (@PathVariable Long id) {
+        userService.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
 }

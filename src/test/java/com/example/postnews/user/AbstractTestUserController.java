0633984a23878190aaada53bcbbf2aed5a6a7731
Alexey Yurchenko <alexeyyurchenko.java@gmail.com>
2024-03-22 16:13:34 +0700

    user test

---
+++ src/test/java/com/example/postnews/user/AbstractTestUserController.java
@@ -0,0 +1,43 @@
+package com.example.postnews.user;
+
+import com.example.postnews.entity.Comment;
+import com.example.postnews.entity.Post;
+import com.example.postnews.entity.User;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.ArrayList;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+public abstract class AbstractTestUserController {
+
+    @Autowired
+    protected MockMvc mockMvc;
+    @Autowired
+    ObjectMapper objectMapper;
+
+    protected User createUser(Long id, Comment comment, Post post) {
+
+        User user = new User(id,"Name " + id,"Name"+id+"@mail.ru",new ArrayList<>(),new ArrayList<>());
+        if (comment != null){
+            user.addComment(comment);
+        }
+        if (post != null){
+            user.addPost(post);
+        }
+        return user;
+    }
+
+
+
+
+
+
+
+}
